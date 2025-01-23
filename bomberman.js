c81e5cc04de62d23e5ebd3647f8979a9b9290fb3
majnun917 <dieye.ndiasse17@gmail.com>
2025-01-24 00:14:42 +0100

    add animation on player move

--- bomberman.js
+++ bomberman.js
@@ -23,6 +23,7 @@ let mapArray = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 ]
 
+
 const heroConfig = {
     tileSize: 32,
     initialGridX: 1, 
@@ -30,96 +31,76 @@ const heroConfig = {
     speed: 1
 };
 
-
-let heroImage = {
-    up: new Image(),
-    down: new Image(),
-    left: new Image(),
-    right: new Image()
+const directions = {
+    up: "ArrowUp",
+    down: "ArrowDown",
+    left: "ArrowLeft",
+    right: "ArrowRight"
 };
 
-heroImage.up.src = './assets/move_up.png';   
-heroImage.down.src = './assets/move_down.png';
-heroImage.left.src = './assets/move_left.png';
-heroImage.right.src = './assets/move_right.png';
-
+const keys = {
+    ArrowUp: directions.up,
+    ArrowDown: directions.down,
+    ArrowLeft: directions.left,
+    ArrowRight: directions.right
+};
 
 class MapHero {
     constructor() {
         this.x = heroConfig.initialGridX * heroConfig.tileSize;
         this.y = heroConfig.initialGridY * heroConfig.tileSize;
         
+      
+        this.heroImgages = {
+            [directions.up]: new Image(),
+            [directions.down]: new Image(),
+            [directions.left]: new Image(),
+            [directions.right]: new Image()
+        };
+
+        this.heroImgages[directions.up].src = './assets/move_up.png';
+        this.heroImgages[directions.down].src = './assets/move_down.png';
+        this.heroImgages[directions.left].src = './assets/move_left.png';
+        this.heroImgages[directions.right].src = './assets/move_right.png';
+        
+     
+        this.heroWidth = 32; 
+        this.heroHeight = 32; 
+
+        this.currentDirection = directions.down;
+        this.frameIndex = 0;
+        this.stepCount = 0;
+        this.stepsPerFrame = 5;
+        
         this.element = document.createElement('div');
-        this.element.style.width = heroConfig.tileSize + "px";
-        this.element.style.height = heroConfig.tileSize + "px";
+        this.element.style.width = `${heroConfig.tileSize}px`;
+        this.element.style.height = `${heroConfig.tileSize}px`;
         this.element.style.position = "absolute";
-        this.element.style.backgroundImage = `url(${heroImage.down.src})`;
+        this.element.style.overflow = "hidden";
         
         mapSence.appendChild(this.element);
-         
-        this.directions_press = [];
-        this.initializeControls(); 
+    
+        this.pressedDirections = [];
+        
+        this.initializeControls();
         this.startGameLoop();
     }
-
     initializeControls() {
         document.addEventListener("keydown", (e) => {
             const dir = keys[e.key];
-            if (dir && this.directions_press.indexOf(dir) === -1) {
-                this.directions_press.unshift(dir);
+            if (dir && this.pressedDirections.indexOf(dir) === -1) {
+                this.pressedDirections.unshift(dir);
             }
         });
 
         document.addEventListener("keyup", (e) => {
             const dir = keys[e.key];
-            const index = this.directions_press.indexOf(dir);
+            const index = this.pressedDirections.indexOf(dir);
             if (index > -1) {
-                this.directions_press.splice(index, 1);
+                this.pressedDirections.splice(index, 1);
             }
         });
     }
-    
-
-    moveHero() {
-        const direction_press = this.directions_press[0];
-        let newX = this.x;
-        let newY = this.y;
-        
-        if (direction_press) {
-            switch(direction_press) {
-                case directions.right:
-                    newX = this.x + heroConfig.speed;
-                    this.element.style.backgroundImage = `url(${heroImage.right.src})`;
-                    break;
-                case directions.left:
-                    newX = this.x - heroConfig.speed;
-                    this.element.style.backgroundImage = `url(${heroImage.left.src})`;
-                    break;
-                case directions.down:
-                    newY = this.y + heroConfig.speed;
-                    this.element.style.backgroundImage = `url(${heroImage.down.src})`;
-                    break;
-                case directions.up:
-                    newY = this.y - heroConfig.speed;
-                    this.element.style.backgroundImage = `url(${heroImage.up.src})`;
-                    break;
-            }
-            
-            if (this.canMove(newX, newY)) {
-                this.x = newX;
-                this.y = newY;
-            }
-        }
-        
-        const mapWidth = mapArray[0].length * heroConfig.tileSize;
-        const mapHeight = mapArray.length * heroConfig.tileSize;
-        
-        this.x = Math.max(heroConfig.tileSize, Math.min(this.x, mapWidth - heroConfig.tileSize));
-        this.y = Math.max(heroConfig.tileSize, Math.min(this.y, mapHeight - heroConfig.tileSize));
-        
-        this.render();
-    }
-    
 
     canMove(newX, newY) {
         const tileSize = heroConfig.tileSize;
@@ -129,7 +110,7 @@ class MapHero {
         if (mapArray[gridY] && ((mapArray[gridY][gridX]) === 1 || (mapArray[gridY][gridX]) === 2)) { 
            
             const topRightX = Math.floor((newX + tileSize - 1) / tileSize);
-            const bottomLeftY = Math.floor((newY + tileSize - 1) / tileSize);
+            const bottomleftY = Math.floor((newY + tileSize - 1) / tileSize);
             const bottomRightX = Math.floor((newX + tileSize - 1) / tileSize);
             const bottomRightY = Math.floor((newY + tileSize - 1) / tileSize);
             console.log(mapArray[gridY][gridX]);
@@ -138,7 +119,7 @@ class MapHero {
                 
                 (mapArray[Math.floor(newY / tileSize)][topRightX] === 1 || 
                  mapArray[Math.floor(newY / tileSize)][topRightX] === 2) &&
-                (mapArray[bottomLeftY][gridX] === 1 || mapArray[bottomLeftY][gridX] === 2) &&
+                (mapArray[bottomleftY][gridX] === 1 || mapArray[bottomleftY][gridX] === 2) &&
                 (mapArray[bottomRightY][bottomRightX] === 1 || 
                  mapArray[bottomRightY][bottomRightX] === 2)
             );
@@ -146,12 +127,59 @@ class MapHero {
         console.log(mapArray[gridY][gridX]);
                        
         return false;
-    }   
+    }    
 
+    moveHero() {
+        const direction = this.pressedDirections[0];
+        let newX = this.x;
+        let newY = this.y;
+        
+        if (direction) {
+            
+            this.stepCount++;
+            if (this.stepCount > this.stepsPerFrame) {
+                this.stepCount = 0;
+                this.frameIndex = (this.frameIndex + 1) % 3;
+            }
+            
+            switch(direction) {
+                case directions.right:
+                    newX = this.x + heroConfig.speed;
+                    this.currentDirection = directions.right;
+                    break;
+                case directions.left:
+                    newX = this.x - heroConfig.speed;
+                    this.currentDirection = directions.left;
+                    break;
+                case directions.down:
+                    newY = this.y + heroConfig.speed;
+                    this.currentDirection = directions.down;
+                    break;
+                case directions.up:
+                    newY = this.y - heroConfig.speed;
+                    this.currentDirection = directions.up;
+                    break;
+            }
+            
+            if (this.canMove(newX, newY)) {
+                this.x = newX;
+                this.y = newY;
+            }
+        } else {
+            this.frameIndex = 0;
+            this.stepCount = 0;
+        }
+        
+        this.render();
+    }
 
     render() {
-        this.element.style.left = this.x + "px";
-        this.element.style.top = this.y + "px";
+        const curHeroDirection = this.heroImgages[this.currentDirection];
+        
+        this.element.style.backgroundImage = `url(${curHeroDirection.src})`;
+        this.element.style.backgroundPosition = `-${this.frameIndex * this.heroWidth}px 0px`;
+        this.element.style.left = `${this.x}px`;
+        this.element.style.top = `${this.y}px`;
     }
 
     startGameLoop() {
@@ -163,22 +191,6 @@ class MapHero {
     }
 }
 
-
-const directions = {
-    up: "ArrowUp",
-    down: "ArrowDown",
-    left: "ArrowLeft",
-    right: "ArrowRight",
-};
-
-const keys = {
-    ArrowUp: directions.up,
-    ArrowLeft: directions.left,
-    ArrowRight: directions.right,
-    ArrowDown: directions.down,
-};
-
-
 window.addEventListener('load', () => {
-    const hero = new MapHero();
+    const hero = new MapHero(mapSence);
 });
